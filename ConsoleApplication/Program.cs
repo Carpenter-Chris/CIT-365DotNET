58c133d5c8485cbdf15e6819904f0a026c8feafd
Carpenter-Chris <49006646+Carpenter-Chris@users.noreply.github.com>
2020-01-11 20:18:41 -0800

    week 01 assignment: first VS Studio C# application
    assignment: Instructions
    1) Create a new Visual C# Console App using .NET Framework project and name it "ConsoleApplication."
    
    2) Store two variables:
            Your name
            Your location (state or country)
    
    3) Output two WriteLine statements that display those two variables with proper labels (My name is ... , I am from ...) using String Interpolation.
    
    4) Output the current date but not the current time.
    
    5) Output the number of days until Christmas this year and, of course, apply an appropriate label to that output.
    
    6) Add the program example from section 2.1 in the C# Programming Yellow Book by Rob Miles.
    
    7) Add these requirements to the code:
        -Provide appropriate text labels when requesting dimensional input.
        -Cause the program to pause in the console so that the application does not automatically terminate when launched from the Visual Studio run debugger tool.
        -Hint: Consider Console.ReadKey()

---
+++ ConsoleApplication/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //variables
+            string userName = "Chris", userLocation = "Los Angeles, CA";
+            DateTime christmas = new DateTime(2020, 12, 25);
+            double christmasCountdown = christmas.Subtract(DateTime.Today).TotalDays;
+
+            //display name and location 
+            Console.WriteLine($"My name is {userName}");
+            Console.WriteLine($"I am from {userLocation}");
+
+            //display date
+            Console.WriteLine("Today is: " + DateTime.Now.ToString("dddd, MMMM dd yyyy"));
+
+            //display number of days until christmas
+            Console.WriteLine($"Days until Christmas: {christmasCountdown}");
+
+            //Program example from section 2.1 of C# Programming Yellow Book
+            double width, height, woodLength, glassArea; 
+            string widthString, heightString;
+
+            Console.WriteLine("To calculate the amount of wood and glass needed to make a window.");
+
+            //takes user input for width of the wood and converts it to a double
+            Console.WriteLine("Please enter the width of the window.");
+            widthString = Console.ReadLine(); 
+            width = double.Parse(widthString);
+
+            //take the user input for height of the wood and converts it to a double
+            Console.WriteLine("Please enter the hight of the window.");
+            heightString = Console.ReadLine(); 
+            height = double.Parse(heightString); 
+
+            //calc the length
+            woodLength = 2 * (width + height) * 3.25; 
+
+            //calc the area
+            glassArea = 2 * (width * height); 
+
+            //output the results
+            Console.WriteLine("The length of the wood is " + woodLength + " feet"); 
+            Console.WriteLine("The area of the glass is " + glassArea + " square metres");
+
+            Console.ReadLine();
+        }
+    }
+}
